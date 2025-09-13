86e93dcd2742036b68d622dec93301e87faa3ef1
hjh <ktorn21@naver.com>
2025-09-13 18:42:34 +0900

    화면 UI 수정

--- nuxt-news-project/app.vue
+++ nuxt-news-project/app.vue
@@ -1,7 +1,14 @@
+<script setup lang="ts">
+import AppHeader from './components/AppHeader.vue';
+import AppNav from './components/AppNav.vue';
+
+</script>
+
 <template>
   <div>
+    <AppHeader />
+    <AppNav />
     <NuxtPage />
-
     
   </div>
 </template>
--- nuxt-news-project/pages/index.vue
+++ nuxt-news-project/pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-    <h1> 메인페이지 입니다.</h1>
+    <div>
+        메인페이지1
+    </div>
 </template>
 
 <script setup lang="ts">
